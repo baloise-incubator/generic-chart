95c9412dfe6adddeeb9de1cfc98159bc095feeb4
Nikolas Philips <nikolas.philips@baloise.ch>
2020-02-04 18:53:03 +0100

    Add release-master Jenkinsfile

---
+++ release-master.groovy
@@ -0,0 +1,44 @@
+@Library('baloise-container-shared-jenkins-library') _
+
+pipeline {
+    agent {
+        label 'podman'
+    }
+
+    options {
+        skipStagesAfterUnstable()
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(numToKeepStr: '28'))
+        timeout(time: 1, unit: 'HOURS')
+        timestamps()
+    }
+
+    stages {
+        stage("Checkout") {
+            steps {
+                gitcheckout url: "https://bitbucket.balgroupit.com/scm/container/generic-chart.git",
+                        branch: "master"
+            }
+        }
+        stage("Helm Push") {
+            steps {
+               helmPush tenant: 'shared', organisation: "openshift"
+            }
+        }
+    }
+
+    post {
+        success {
+            notifyBitBucket state: "SUCCESSFUL"
+        }
+
+        fixed {
+            mailTo status: "SUCCESS", actuator: true, recipients: [], logExtract: true
+        }
+
+        failure {
+            notifyBitBucket state: "FAILED"
+            mailTo status: "FAILURE", actuator: true, recipients: [], logExtract: true
+        }
+    }
+}
